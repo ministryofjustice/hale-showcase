2cd31d66a16cef66bfeb1771e11be9a26a2d2a14
adam <brown-a2@users.noreply.github.com>
2024-06-27 12:58:08 +0100

    Init build of child theme

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+const mix_ = require('laravel-mix');
+
+
+mix_.setPublicPath('./dist')
+  .copy('./assets/images/*', 'dist/images/')
+  .copy('./assets/webfonts/*', 'dist/webfonts/')
+  .sass('./assets/scss/style.scss', 'css/hale-showcase-style.min.css')
+  .copy('./assets/js/*', 'dist/js/')
+  .options({
+    processCssUrls: false
+  });
+
+if (mix_.inProduction()) {
+  mix_.version();
+} else {
+  mix_.sourceMaps();
+}
