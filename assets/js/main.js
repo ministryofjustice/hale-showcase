56dcdabcffe5fdc99edb23490deee1f91a285154
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-07-09 12:01:08 +0100

    Redid colour choosing options, added options

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Hale Showcase theme JS
 
 function changeColour(colourNumber,totalColours) {
-	let html = document.getElementById("hale-showcase-html");
+	let html = document.getElementsByTagName("html")[0];
 	// remove all colour classes ready for new one
 	if (html.classList.length > 1) {
 		for (let i=0;i<=totalColours;i++) {
--- partials/colour-picker.php
+++ partials/colour-picker.php
@@ -1,7 +1,7 @@
 <?php
 // Form options
-$colour_choice_text = __("Example colour scheme ", 'hale'); //will be followed by number
-$colour_options_count = 4; //how many options to give
+$colour_choice_text = __("Example ", 'hale'); //will be followed by number
+$colour_options_count = 8; //how many options to give
 ?>
 
 <div class="showcase-top-colour-changer">
@@ -9,32 +9,32 @@ $colour_options_count = 4; //how many options to give
     <details class="govuk-details govuk-!-margin-bottom-0">
       <summary class="govuk-details__summary">
         <span class="govuk-details__summary-text">
-          Expore colour options
+          Explore colour options
         </span>
       </summary>
-      <div class="govuk-details__text">
+      <div class="govuk-details__text govuk-!-margin-bottom-6">
         <form action="." method="post" novalidate>
-          <div class="govuk-form-group">
-            <fieldset id="colour-picker-top-form" class="govuk-fieldset govuk-!-margin-bottom-7">
+          <div class="govuk-form-group  govuk-!-margin-bottom-1">
+            <fieldset id="colour-picker-top-form" class="govuk-fieldset">
               <legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
                 <h2 class="govuk-fieldset__heading">
                   Choose an example colour scheme
                 </h2>
               </legend>
-              <div class="govuk-radios" data-module="govuk-radios">
-                <div class="govuk-radios__item">
+              <div class="govuk-radios govuk-radios--small govuk-radios--inline" data-module="govuk-radios">
+                <div class="govuk-radios__item showcase-colour-radio">
                   <input class="govuk-radios__input" id="colour-0" name="colour" type="radio" value="0" checked  onclick='changeColour("0",<?php echo $colour_options_count; ?>);'>
                   <label class="govuk-label govuk-radios__label" for="colour">
-                    Remove example colour scheme
+                    Remove example
                   </label>
                 </div>
                 <?php
                   for ($i=1;$i<=$colour_options_count;$i++) {
                     echo "
-                    <div class='govuk-radios__item'>
+                    <div class='govuk-radios__item showcase-colour-radio'>
                       <input class='govuk-radios__input showcase-colour-option' id='colour-$i' name='colour' type='radio' value='$i' onclick='changeColour($i,$colour_options_count);' >
                       <label class='govuk-label govuk-radios__label' for='colour'>
-                        $colour_choice_text $i
+                        <span class='showcase-example-colour-label'>$colour_choice_text $i</span>
                         <span class='showcase-example-colour-preview showcase-example-colour-preview--$i'>
                           <span> </span><span> </span><span> </span>
                         </span>
